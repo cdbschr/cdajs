923a7d7f6bb81c3e678fd67d8fa1f5aadde79ea0
cdbschr <c.debusscher@gmail.com>
2022-06-28 15:45:59 +0200

    👌 IMPROVE: alert

--- script.js
+++ script.js
@@ -7,26 +7,14 @@ let inputValue = parseInt(input.value);
 //Je récupère le bouton
 let button = document.getElementById("button");  
 
-//Je définis mes valeurs pour la musique
-const valueMin = 0;
-const valueOne = 1;
-const valueTwo = 2;
-const valueMax = 99;
-//Je récupère mes valeurs dans le html
-let number = document.getElementsByClassName("number");
-let magasin = document.getElementById("magasin");
-let plusieurs = Array.from(document.getElementsByClassName("plusieurs"));
-let minusOne = document.getElementsByClassName("numberMinusOne");
-//Contient un petit texte humoristique
-let alert = document.getElementById("alert");
-
-//Le bouton va appeler la fonction display
+//Le click sur le boutton va créer item dans le sessionStorage et recharger la page afin d'afficher les valeurs
 button.addEventListener("click", () => {
   sessionStorage.setItem("restart", "true");
 
   document.location.reload();
 });
 
+//Fonction qui chercher un item dans le sessionStorage et qui vérifie qu'il est bien supprimé lors du reload afin d'afficher les valeurs
 window.onload = () => {
   let restart = sessionStorage.getItem("restart");
 
@@ -36,16 +24,32 @@ window.onload = () => {
   }
 }
 
-// Je crée mes conditions par rapport à la valeur de l'input
+// Je crée mes conditions par rapport à la valeur de l'input dans une fonction
 function display() {
+  //Je définis mes valeurs pour la musique
+  const valueMin = 0;
+  const valueOne = 1;
+  const valueTwo = 2;
+  const valueMax = 99;
+  //Je récupère mes valeurs dans le html
+  let number = document.getElementsByClassName("number");
+  let magasin = document.getElementById("magasin");
+  let plusieurs = Array.from(document.getElementsByClassName("plusieurs"));
+  let minusOne = document.getElementsByClassName("numberMinusOne");
+  let p = document.getElementsByTagName("p");
+
   if (inputValue > valueMax) {
-    alert.innerText = "Oulaa ! Ça fait un peu beaucoup là, non ? Le stock maximum est de 99 bolées.";
+    //je définis au premier paragraph un id pour la couleur de fond
+    p[0].setAttribute("id", "alert");
+    //et lui rajoute un texte humoristique
+    p[0].innerText = "Oulaa ! Ça fait un peu beaucoup là, non ? Le stock maximum est de 99 bolées.";
     number[0].innerText = valueMax;
     number[1].innerText = valueMax;
     minusOne[0].innerHTML = valueMax - valueOne;
 
   } else if (inputValue < valueMin) {
-    alert.innerText = "C'était sans alcool il me semble ?";
+    p[0].setAttribute("id", "alert");
+    p[0].innerText = "C'était peut-être pas sans alcool. Un inventaire est nécessaire.";
     number[0].innerText = valueMin;
     number[1].innerText = valueMin;
     minusOne[0].innerHTML = valueMax;
